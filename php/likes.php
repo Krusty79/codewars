22413c4ddc0816ac47f37fc478f9b7ba473fd36c
romaintegrio <romaintegrio@gmail.com>
2022-11-18 18:31:46 +0200

    smarteest solution

--- php/likes.php
+++ php/likes.php
@@ -23,7 +23,15 @@
         }
     }
     */
-    function likes( $names ) {
+    function likes($names): string {
+        return array(
+          '0' => "no one likes this",
+          '1' => "{$names[0]} likes this",
+          '2' => "{$names[0]} and {$names[1]} like this",
+          '3' => "{$names[0]}, {$names[1]} and {$names[2]} like this",
+          '4' => "{$names[0]}, {$names[1]} and ". (sizeof($names) - 2) ." others like this")[min(4, sizeof($names))];
+    }
+    function _likes( $names ) {
 
         // Your code here...
         $likes="";
