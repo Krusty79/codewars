a264704e4c385e9c995d0e6f14b4496c09a44dc6
romaintegrio <romaintegrio@gmail.com>
2023-03-23 16:23:41 +0200

    openAI

---
+++ python/openAi.py
@@ -0,0 +1,42 @@
+import openai,re,sys
+openai.api_key="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"
+"""
+response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+            {"role": "system", "content": "You are a chatbot"},
+            {"role": "user", "content": "Why should DevOps engineer learn kubernetes?"},
+        ]
+)
+
+result = ''
+for choice in response.choices:
+    result += choice.message.content
+
+print(result)
+"""
+
+prompt = input("request:")
+
+if len(prompt) < 10 :
+    print("request must be a least 10 symbols")
+    sys.exit()
+
+# Set up the model and prompt
+model_engine = "text-davinci-003"
+#prompt = "Гданск во время второй мировой войны..."
+
+# Generate a response
+completion = openai.Completion.create(
+    engine=model_engine,
+    prompt=prompt,
+    max_tokens=2048,
+    n=6,
+    stop=None,
+    temperature=0.5,
+)
+
+for resp in completion.choices:
+    print('<p>{0}</p>'.format(re.sub(r"[\n\t]*", "", resp.text)))
+
+#.replace("\n", " ")
