1ee0c2894f99875a5d611e32d382f3e412dbc286
romaintegrio <romaintegrio@gmail.com>
2023-05-16 22:27:45 -0500

    opposite

---
+++ python/opposite.py
@@ -0,0 +1,20 @@
+"""
+https://www.codewars.com/kata/56dec885c54a926dcd001095/train/python
+Very simple, given an integer or a floating-point number, find its opposite.
+
+Examples:
+
+1: -1
+14: -14
+-34: 34
+"""
+
+
+def opposite(number):
+    # your solution here
+    return number*-1
+
+
+print(opposite(1))
+print(opposite(14))
+print(opposite(-34))
