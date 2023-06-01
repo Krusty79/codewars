b7d6b7edd1c374231079ee5ae679fd3643d6d75d
romaintegrio <romaintegrio@gmail.com>
2023-05-31 23:09:39 -0500

    noop

--- c#/codewars/Program.cs
+++ c#/codewars/Program.cs
@@ -1,3 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 new Kata.ToCamelCase("the_stealth_warrior");
+new Kata.ToCamelCase("The_Stealth_Warrior");
+new Kata.ToCamelCase("The_Stealth-Warrior");
